c2c98d9c3698d8423afd878ce77d9eb5938675bc
superloyt <loisejiele@yahoo.com>
2017-07-24 21:36:35 +0800

    edited and added by loise

---
+++ login.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="TUP Online Election">
+  <title>TUP Online Election</title>
+  <link href="dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="dist/css/bootstrap.css" rel="stylesheet" />
+  <link href="dist/js/bootstrap.min.js" rel="stylesheet" />
+  <link href="dist/js/bootstrap.js" rel="stylesheet" />
+  <link href="dist/js/adminlte.js" rel="stylesheet" />
+  <link href="dist/js/adminlte.min.js" rel="stylesheet" />
+  <link href="dist/css/AdminLTE.css" rel="stylesheet" />
+  <link href="dist/css/AdminLTE.min.css" rel="stylesheet" />
+  <link href="dist/css/font-awesome.min.css" rel="stylesheet" />
+  <link href="dist/css/ionicons.min.css" rel="stylesheet" />
+
+  <link href="assets/css/tup-main.css" rel="stylesheet" />
+  <link href="assets/css/tup-media.css" rel="stylesheet" />
+
+</head>
+<body>
+
+</body>
+</html>
--- navbar-user.php
+++ navbar-user.php
@@ -10,7 +10,13 @@
   <link href="dist/css/bootstrap.css" rel="stylesheet" />
   <link href="dist/js/bootstrap.min.js" rel="stylesheet" />
   <link href="dist/js/bootstrap.js" rel="stylesheet" />
+  <link href="dist/js/adminlte.js" rel="stylesheet" />
+  <link href="dist/js/adminlte.min.js" rel="stylesheet" />
+  <link href="dist/css/AdminLTE.css" rel="stylesheet" />
+  <link href="dist/css/AdminLTE.min.css" rel="stylesheet" />
   <link href="assets/css/tup-main.css" rel="stylesheet" />
+  <link href="assets/css/tup-media.css" rel="stylesheet" />
+
 </head>
 <body>
   <div class="container-fluid">
