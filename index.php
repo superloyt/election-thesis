c09a37fbba0b7c90a5199d7d0236d6e42140c03b
superloyt <loisejiele@yahoo.com>
2017-07-24 15:50:45 +0800

    edited and added by loise

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="TUP Online Election">
+  <title>TUP Online Election</title>
+  <link href="dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="dist/js/bootstrap.min.js" rel="stylesheet" />
+  <link href="assets/css/tup-main.css" rel="stylesheet" />
+</head>
+<body>
+  <?php
+  include "navbar.php";
+   ?>
+</body>
+</html>
