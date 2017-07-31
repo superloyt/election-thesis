31f95042bf086d66c906b4b1fba98911b190aacf
superloyt <loisejiele@yahoo.com>
2017-07-31 15:45:08 +0800

    edited by loise

--- navbar-guest.php
+++ navbar-guest.php
@@ -33,7 +33,7 @@
         <ul class="nav navbar-nav">
           <li><a href="index.php">Home</a></li>
           <li class="dropdown">
-            <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Candidates <span class="caret"></span></a>
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Candidates <span class="caret"></span></a>
             <ul class="dropdown-menu">
               <li><a href="#">President</a></li>
               <li><a href="#">Vice President</a></li>
--- register.php
+++ register.php
@@ -38,22 +38,22 @@
       <p class="login-box-msg">REGISTER NOW!</p>
 
       <form action="home.php" method="post">
-        <div class="form-group">
+        <div class="form-group has-feedback">
           <input type="name" name="student-id" class="form-control" placeholder="STUDENT ID">
         </div>
-        <div class="form-group">
+        <div class="form-group has-feedback">
           <input type="email" name="email" class="form-control" placeholder="EMAIL">
         </div>
-        <div class="form-group">
+        <div class="form-group has-feedback">
           <input type="name" name="firstname" class="form-control" placeholder="FIRST NAME">
         </div>
-        <div class="form-group">
+        <div class="form-group has-feedback">
           <input type="name" name="midname" class="form-control" placeholder="MIDDLE NAME">
         </div>
-        <div class="form-group">
+        <div class="form-group has-feedback">
           <input type="name" name="lastname" class="form-control" placeholder="LAST NAME">
         </div>
-        <div class="form-group">
+        <div class="form-group has-feedback">
           <select class="form-control select2">
             <option value="" selected="selected" disabled="disabled">SELECT YOUR COLLEGE</option>
             <option value="CAFA">COLLEGE OF ARCHITECTURE AND FINE ARTS</option>
@@ -62,6 +62,16 @@
             <option value="CIT">COLLEGE OF INDUSTRIAL TECHNOLOGY</option>
             <option value="CLA">COLLEGE OF LIBERAL ARTS</option>
             <option value="COS">COLLEGE OF SCIENCE</option>
+          </select>
+        </div>
+        <div class="form-group has-feedback">
+          <select class="form-control select2">
+            <option value="" selected="selected" disabled="disabled">SELECT YOUR COURSE</option>
+            <option value="BSIT">BACHELOR OF SCIENCE IN INFORMATION TECHNOLOGY (BSIT)</option>
+            <option value="BSIS">BACHELOR OF SCIENCE IN INFORMATION SYSTEM (BSIS)</option>
+            <option value="BSCS">BACHELOR OF SCIENCE IN COMPUTER SCIENCE (BSCS)</option>
+            <option value="BS-ES">BACHELOR OF SCIENCE IN ENVIRONMENTAL SCIENCE (BS-ES)</option>
+            <option value="BAS-LT">BACHELOR IN APPLIED SCIENCE MAJOR IN LABORATOTY TECHNOLOGY (BAS-LT)</option>
           </select>
         </div>
         <div class="row">
